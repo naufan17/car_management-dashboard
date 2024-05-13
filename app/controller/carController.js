8ee8d2949d9d4618b9506ed0b4c372cac207f68f
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-05-13 15:21:44 +0700

    initialization project

---
+++ app/controller/carController.js
@@ -0,0 +1,16 @@
+const cloudinary = require('../config/cloudinary');
+const Car = require('../models/Car')
+
+const getCar = async (req, res) => {}
+
+const getCarById = async (req, res) => {}
+
+const createCar = async (req, res) => {}
+
+const uploadImageCarCar = async (req, res) => {}
+
+const updateCar = async (req, res) => {}
+
+const deleteCar = async (req, res) => {}
+
+module.exports = { getCar, getCarById, createCar, uploadImageCarCar, updateCar, deleteCar }
